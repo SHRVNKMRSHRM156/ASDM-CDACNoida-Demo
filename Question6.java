2350d4b334d32a306d26b200b9316ff189ed7ccc
SHRVNKMRSHRM156 <60319549+SHRVNKMRSHRM156@users.noreply.github.com>
2023-03-27 10:57:25 +0530

    Uploading of Java Code Text file.
    This is an upload of Java Code Text File. I have uploaded question 6 of COP Assignment 2.

---
+++ Question6.java
@@ -0,0 +1,16 @@
+/*
+ * Q6: Write a program to print Fibonacci series 
+ * of n terms where n is input by user :
+ * 0 1 1 2 3 5 8 13 24 .....
+ */
+
+package assignment2;
+import
+public class Question6 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
